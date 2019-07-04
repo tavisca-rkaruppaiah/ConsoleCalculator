bed928d674ab20b27798a3417ef40b11f40bdb2f
tavisca-rkaruppaiah <rkaruppaiah@tavisca.in>
2019-07-04 19:58:41 +0530

    Added backup for ConsoleCalculator

---
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -0,0 +1,91 @@
+using System;
+using Xunit;
+
+namespace ConsoleCalculator.Tests
+{
+    public class CalculatorFixture
+    {
+        Calculator calculator;
+
+        public CalculatorFixture()
+        {
+            calculator = new Calculator();
+        }
+
+
+        [Fact]
+        public void Single_KeyPress_With_Value_0_Should_Return_0()
+        {
+            string pressedKeys = "0";
+            Assert.Equal("0", OriginalResult(pressedKeys));
+        }
+
+        [Fact]
+        public void Multiple_KeyPress_With_Value_123_Should_Return_123()
+        {
+            string pressedKeys = "123";
+            Assert.Equal("123", OriginalResult(pressedKeys));
+        }
+
+        [Fact]
+        public void Multiple_KeyPress_With_Value_000_Should_Return_0()
+        {
+            string pressedKeys = "0000";
+            Assert.Equal("0", OriginalResult(pressedKeys));
+        }
+
+
+        [Fact]
+        public void Error_Calculator_Should_Return_E()
+        {
+            string pressedKeys = "10/0=";
+            Assert.Equal("-E-", OriginalResult(pressedKeys));
+        }
+
+        [Fact]
+        public void PerformAdditionOperation()
+        {
+            string pressedKeys = "10+2=";
+            Assert.Equal("12", OriginalResult(pressedKeys));
+        }
+
+
+        [Fact]
+        public void PerformDecimalAdditionOperation()
+        {
+            string pressedKeys = "1..1+2..1=";
+            Assert.Equal("3.2", OriginalResult(pressedKeys));
+        }
+
+        
+
+        [Fact]
+        public void PerformSignChangeOperation()
+        {
+            string pressedKeys = "1s";
+            Assert.Equal("-1", OriginalResult(pressedKeys));
+        }
+
+        [Fact]
+        public void PerformClearCalculator_ShouldReturnWith_0()
+        {
+            string pressedKeys = "1+2+3c";
+            Assert.Equal("0", OriginalResult(pressedKeys));
+        }
+
+
+
+
+
+        private string OriginalResult(string expression)
+        {   
+            string result = null;
+
+            foreach (char keys in expression)
+            {
+                result = calculator.SendKeyPress(keys);
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,258 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        string userPressedKey = "";
+        string firstPart = "";
+        char operand;
+        string result;
+        bool mAddition = false, mSubtraction = false, mMultiplication = false, mDivision = false;
+        public string SendKeyPress(char key)
+        {
+
+
+
+            if (char.IsLetter(key))
+            {
+                key = char.ToUpper(key);
+
+                switch (key)
+                {
+                    case 'C':
+                        return ClearCalculator();
+
+                    case 'S':
+                        userPressedKey = ChangeSign(userPressedKey);
+                        break;
+
+                    case 'X':
+                        if (mDivision == false)
+                        {
+                            result = SetVariableValues(userPressedKey, '*');
+                            mDivision = true;
+                            return result;
+                        }
+                        else
+                        {
+                            result = PerformOperations(result, userPressedKey, '*');
+                            operand = '*';
+                            return result;
+                        }
+
+                }
+            }
+            if (char.IsDigit(key))
+            {
+                switch (key)
+                {
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        userPressedKey += key;
+                        break;
+                    case '0':
+                        userPressedKey = CheckRepeationZero(userPressedKey, key);
+                        break;
+
+                }
+            }
+            else
+            {
+                switch (key)
+                {
+                    case '+':
+                        if (mAddition == false)
+                        {
+                            result = SetVariableValues(userPressedKey, key);
+                            mAddition = true;
+                            return result;
+
+                        }
+                        else
+                        {
+                            result = PerformOperations(result, userPressedKey, key);
+                            operand = key;
+                            return result;
+
+                        }
+
+                    case '-':
+                        if (mSubtraction == false)
+                        {
+                            result = SetVariableValues(userPressedKey, key);
+                            mSubtraction = true;
+                            return result;
+
+                        }
+                        else
+                        {
+                            result = PerformOperations(result, userPressedKey, key);
+                            operand = key;
+                            return result;
+                        }
+
+
+                    case '/':
+                        if (mDivision == false)
+                        {
+                            result = SetVariableValues(userPressedKey, key);
+                            mDivision = true;
+                            return result;
+
+                        }
+                        else
+                        {
+                            result = PerformOperations(result, userPressedKey, key);
+                            operand = key;
+                            return result;
+
+                        }
+
+                    case '=':
+                        result = PerformOperations(result, userPressedKey, operand);
+                        return result;
+
+
+                    case '.':
+                        userPressedKey = CheckRepeationDot(userPressedKey, key);
+                        break;
+
+                }
+            }
+
+
+            return userPressedKey;
+        }
+
+
+
+        public string SetVariableValues(string firstNumber, char operationSymbol)
+        {
+            if (CheckStringIsNullOrNot(firstNumber))
+            {
+
+                firstPart = firstNumber;
+                operand = operationSymbol;
+                userPressedKey = "";
+                return firstNumber;
+            }
+
+            return firstNumber;
+        }
+
+        public string CheckRepeationZero(string expression, char key)
+        {
+
+            if (userPressedKey.Equals("0"))
+            {
+                return userPressedKey;
+            }
+
+            else
+            {
+                userPressedKey += key;
+            }
+            return userPressedKey;
+        }
+
+        public string CheckRepeationDot(string expression, char key)
+        {
+
+            if (CheckStringIsNullOrNot(expression))
+            {
+
+                if (expression.Contains("."))
+                {
+                    return expression;
+                }
+                else
+                {
+                    return expression += key;
+                }
+            }
+            else
+            {
+                expression = "";
+            }
+            return expression;
+        }
+        public string ClearCalculator()
+        {
+            userPressedKey = "";
+            operand = char.MinValue;
+            firstPart = null;
+            result = null;
+            mAddition = mSubtraction = mMultiplication = mDivision = false;
+
+            return "0";
+
+
+        }
+
+        public bool CheckStringIsNullOrNot(string expression)
+        {
+            if (expression.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string ChangeSign(string expression)
+        {
+            double signNumber = 0.0;
+            if (CheckStringIsNullOrNot(expression))
+            {
+                double.TryParse(expression, out signNumber);
+
+                signNumber = signNumber * -1;
+            }
+
+            return signNumber.ToString();
+        }
+
+        public string PerformOperations(string numberOne, string numberTwo, char operandSymbol)
+        {
+            userPressedKey = "";
+            double number1, number2, result = 0.0;
+            double.TryParse(numberOne, out number1);
+            double.TryParse(numberTwo, out number2);
+
+            switch (operandSymbol)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '/':
+                    if (number2 > 0)
+                    {
+                        result = number1 / number2;
+                    }
+                    else
+                    {
+                        ClearCalculator();
+                        return "-E-";
+                    }
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+            }
+
+
+            return result.ToString();
+        }
+    }
+}
